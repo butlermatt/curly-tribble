b4ea968dee936ca66478d0f2cbf14012a598d271
Matthew Butler <butler.matthew@gmail.com>
2015-11-05 22:59:09 -0400

    Initial scaffolding

---
+++ lib/slide_deck.dart
@@ -0,0 +1,10 @@
+@HtmlImport('slide_deck.html')
+library curly.tribble.slide.deck;
+
+import 'package:polymer/polymer.dart';
+import 'package:web_components/web_components.dart';
+
+@PolymerRegister('slide-deck')
+class SlideDeck extends PolymerElement {
+  SlideDeck.created() : super.created();
+}
---
+++ web/index.dart
@@ -0,0 +1,9 @@
+library curly.tribble;
+
+import 'package:polymer/polymer.dart';
+
+import 'package:curly_tribble/slide_deck.dart';
+
+void main() {
+  initPolymer();
+}
